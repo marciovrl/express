21bbcf20154478fa45b162da0158e033152e67c8
marcio lemos <contato.marciovrl@gmail.com>
2020-07-29 15:39:13 -0300

    edit logic filter

--- src/services/cars.js
+++ src/services/cars.js
@@ -3,7 +3,10 @@ const cars = require('../../data/cars.json');
 module.exports = () => {
   const findAll = async () => cars;
 
-  const find = async (filter = {}) => cars[filter.id];
+  const find = async (filter = {}) => {
+    const car = cars.filter((item) => item.id === Number(filter.id));
+    return car;
+  };
 
   return { findAll, find };
 };
