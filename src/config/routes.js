4e86afadda4c8f24031dcb5d575a41e891485e12
marcio lemos <contato.marciovrl@gmail.com>
2020-07-31 15:03:50 -0300

    add users endpoint routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,10 @@ const PATH = '/cars-quiz';
 
 module.exports = (app) => {
   app.route(`${PATH}/cars`).get(app.routes.cars.findAll);
-
   app.route(`${PATH}/cars/:id`).get(app.routes.cars.find);
+  app.route(`${PATH}/users`).get(app.routes.users.findAll);
+  app.route(`${PATH}/users/:id`).get(app.routes.users.find);
+  app.route(`${PATH}/users`).post(app.routes.users.create);
+  app.route(`${PATH}/users/:id`).put(app.routes.users.update);
+  app.route(`${PATH}/users/:id`).delete(app.routes.users.remove);
 };
