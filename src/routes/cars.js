0a371e59966b585ceb424045fccb51c1a10731cf
marcio lemos <contato.marciovrl@gmail.com>
2020-07-31 15:18:16 -0300

    fix /cars/{id}

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
   const find = (req, res) => {
     app.services.cars
       .find({ id: req.params.id })
-      .then((result) => res.status(200).json(result));
+      .then(([result]) => res.status(200).json(result));
   };
 
   return { findAll, find };
